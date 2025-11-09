1fba32a9380c89f64899e67a89c74b11a283b28f
yash garg <yg292001@gmail.com>
2025-11-09 13:41:40 +0530

    added resposiveness for mobile devices

--- src/sections/AboutTeam.tsx
+++ src/sections/AboutTeam.tsx
@@ -2,14 +2,14 @@ function AboutTeam() {
   return (
     <div id="about-team" className="h-dvh flex flex-col md:flex-row snap-start justify-start md:justify-around relative gap-10 sm:gap-2">
 
-      <div className="md:w-9/20 lg:w-1/2 flex flex-col items-center">
+      <div className="md:w-8/20 lg:w-1/2 flex flex-col items-center">
         <img className="relative w-60 h-50 sm:h-60 sm:w-80 md:h-80 md:w-110 lg:w-120 lg:h-105 z-60 lg:ml-5" src={"/assets/AboutTeamStickyNote.svg"} />
         <div className="hidden md:flex items-start justify-around">
           <img src={"/assets/IndiaGate.svg"} className="size-90" />
         </div>
       </div>
 
-      <div className="md:w-11/20 lg:w-1/2 flex flex-col justify-center items-center">
+      <div className="md:w-12/20 lg:w-1/2 flex flex-col justify-center items-center">
         <div className="md:mt-10 lg:mt-5 flex justify-center md:justify-start w-full">
           <p className="text-lg sm:text-2xl md:text-4xl text-black font-island-moments">
             Film Makers <img src={"/assets/DownPointingVector.svg"} className="ml-20 h-12 w-17 md:h-24 md:w-36" />
@@ -23,7 +23,7 @@ function AboutTeam() {
             Branding Experts
           </p>
 
-          <img className="h-30 w-50 sm:h-45 sm:w-70 md:h-80 md:w-70 lg:w-100" src={"/assets/PersonStanding.svg"} />
+          <img className="h-30 w-50 sm:h-45 sm:w-70 md:h-50 md:w-60 lg:h-70 lg:w-80 xl:h-90 xl:w-100" src={"/assets/PersonStanding.svg"} />
 
           <p className="flex-1 text-lg sm:text-2xl md:text-4xl text-black font-island-moments">
             Art Curators
@@ -32,7 +32,7 @@ function AboutTeam() {
         </div>
 
         <div className="flex flex-col gap-3 justify-center items-center">
-          <p className="text-lg md:text-4xl font-halant">Take a closer look at the stories V bring to life.</p>
+          <p className="text-lg md:text-xl lg:text-2xl xl:text-3xl font-halant">Take a closer look at the stories V bring to life.</p>
           <a className="border rounded-3xl text-white bg-orange-500 px-3 py-2 hover:bg-orange-600 hover:scale-105 transition-transform" href="#portfolio">View Portfolio</a>
         </div>
       </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,13 +2,13 @@ function Hero() {
   return (
     <div id="hero" className="h-dvh flex flex-col lg:flex-row snap-start justify-around items-center">
       <div className="lg:w-1/2 grid place-items-center mt-10 md:mt-0">
-        <img src={"/assets/Hero Mandala.svg"} className="size-60 md:size-130 col-start-1 row-start-1" alt="Hero Mandala" />
-        <img src={"/logo-high.png"} className="h-12 w-36 md:h-36 md:w-96 col-start-1 row-start-1" alt="Hero Mandala" />
+        <img src={"/assets/Hero Mandala.svg"} className="size-60 sm:size-90 md:size-100 lg:size-130 col-start-1 row-start-1" alt="Hero Mandala" />
+        <img src={"/logo-high.png"} className="h-12 w-36 sm:h-20 sm:w-60 lg:h-36 lg:w-96 col-start-1 row-start-1" alt="Hero Mandala" />
       </div>
-      <div className="lg:w-1/2 px-10 flex flex-col gap-10 text-center">
-        <div className="font-island-moments text-2xl md:text-3xl lg:text-[80px]">Varnan is where stories find their voice and form</div>
-        <div className="text-lg lg:text-[36px] text-[#F15D2B]">Films . Brands . Art</div>
-        <div className="text-base lg:mx-15">Since 2009, V’ve been telling stories - stories of people, their journeys, and the places that shape them.
+      <div className="lg:w-1/2 px-10 flex flex-col gap-5 lg:gap-10 text-center">
+        <div className="font-island-moments text-2xl sm:text-3xl md:text-4xl lg:text-[80px]">Varnan is where stories find their voice and form</div>
+        <div className="text-lg md:text-2xl lg:text-[36px] text-[#F15D2B]">Films . Brands . Art</div>
+        <div className="text-base sm:text-lg lg:mx-15">Since 2009, V’ve been telling stories - stories of people, their journeys, and the places that shape them.
           Some begin in polished boardrooms, others in humble village squares. But every story starts the same way - by listening with intention. V believes it takes trust, patience, and an eye for the unseen to capture what truly matters.
           V doesn’t just tell stories - V honors them.</div>
       </div>
